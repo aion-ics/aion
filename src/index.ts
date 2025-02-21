a45c8d19f0e44360252fb9a2ce8a5ad38fc3ee31
Timur Cravtov <timurcravtov04@gmail.com>
2025-02-21 16:45:37 +0200

    initial

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// language.ts
+import * as fs from 'fs';
+
+// Create example.txt with some content
+fs.writeFileSync('example.txt', 'Hello from my language!');
+
+console.log('Created example.txt successfully');
+console.log('hi');

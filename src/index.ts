f59fad1ddf69a778e6638df44b52d573bdf1a590
Timur Cravtov <timurcravtov04@gmail.com>
2025-02-23 10:26:38 +0200

    found a way to use colours

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-// language.ts
 import * as fs from 'fs';
+const chalk = require('chalk'); // Use require() with chalk@4
 
-// Create example.txt with some content
 fs.writeFileSync('example.txt', 'Hello from my language!');
 
-console.log('Created example.txt successfully');
-console.log('hi');
+console.log(chalk.green('✅ Created example.txt successfully.'));
+console.error(chalk.red('❌ Created example.txt unsuccessfully!'));
+
+throw new Error(chalk.bgRed.white("Something went wrong!"));
+
+console.log(chalk.blue('hi'));

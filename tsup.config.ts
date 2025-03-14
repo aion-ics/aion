a76585fe6817c66e23a29a506ff83319c559cdcf
Timur Cravtov <timurcravtov04@gmail.com>
2025-03-14 15:02:56 +0200

    added grammar to structure

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/cli.ts'], // Include cli.ts as an entry point
-  format: ['cjs', 'esm'], // Build for both CommonJS and ES Modules
+  entry: ['src/index.ts', 'src/cli.ts', 'src/playground.ts'],
+    format: ['cjs', 'esm'], // Build for both CommonJS and ES Modules
   dts: true, // Generate .d.ts type definitions
   splitting: false, // Disable code splitting (optional)
   clean: true, // Clean the output directory before building

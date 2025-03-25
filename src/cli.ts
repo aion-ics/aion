f6866b59a600b010afcd62462b30e1326bcfb7a7
Timur Cravtov <timurcravtov04@gmail.com>
2025-03-25 12:21:12 +0200

    added cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,16 @@
 import chalk from 'chalk';
 
-console.log(chalk.red("I'm from CLI"))
+// @ts-ignore
+import packageJson from '../package.json';
 
 const args = process.argv;
 
-console.log(args)
+if (args.length == 2) {
+    const version = packageJson.version;
+    console.log("This is " + chalk.blue("Aion") + " version " + packageJson.version );
+    console.log("Please provide an " + chalk.blue(".aion") + " file to get it compiled.");
+    console.log("The documentation is available at https://aion-ics.github.io/" )
+
+} else {
+    console.log(args.at(2));
+}

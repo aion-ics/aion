16475889e2458b4c42d1bd1ae97b2bafb197cf7e
Timur Cravtov <timurcravtov04@gmail.com>
2025-04-21 14:08:01 +0300

    Updated the CLI architecture

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -18,16 +18,35 @@ if (args.length == 2) {
     console.log("Please provide an " + chalk.blue(".aion") + " file to get it compiled.");
     console.log("The documentation is available at https://aion-ics.github.io/" )
 
+// this part will handle compiler flags and the compilation process itself
 } else {
 
     const arg = args.at(2)
 
-    if (arg === "--version") {
-        console.log("Aion " + version)
-    } else {
+    switch(arg) {
+        case "--version":
+        case "-v":
+            console.log("Aion " + version);
+            break;
+        case "--help":
+        case "-h":
+            displayHelpMessageCLI();
+            break;
+        default:
+            handleDefaultArgumentCLI(arg);
+            break;
+    }
+}
+
+function handleDefaultArgumentCLI(arg: string) {
+    if (arg.endsWith(".aion")) {
+        // handle the compilation
         const file = fs.readFileSync(arg, 'utf-8');
         console.log(file);
-
     }
 
+}
+
+function displayHelpMessageCLI() {
+    console.log("No help, no mercy")
 }

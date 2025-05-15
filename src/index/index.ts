d0b6c67ff296bec28fd000f36527fbd333870b6d
Timur Cravtov <timurcravtov04@gmail.com>
2025-05-15 13:36:34 +0300

    updated entry npm func

--- src/index/index.ts
+++ src/index/index.ts
@@ -5,8 +5,19 @@
 import { AionLanguageExecutor } from "../core/AionLanguageExecutor";
 import { IODictionarySystem } from "../core/intepreter/helpers/io_system/ioDictionarySystem";
 
-export function runAionCode(code: string, fileName: string = "input.aion") {
+
+/**
+ * 
+ * @param code The Aion source code to be executed.
+ * @param filename The name of the file containing the Aion source code.
+ * @param existingCalendars - A map of existing calendars (.ics) to be used with filename as a key (without the .ics extension) and the calendar code as a value.
+ * @returns updated and created calendars in form of the map. 
+ */
+export function executeAionCode(code: string, filename: string = "input.aion", existingCalendars: Map<String, String>): Map<String, String> {
     const ioSystem = new IODictionarySystem(null);
     const aionLanguageExecutor = new AionLanguageExecutor(ioSystem);
     aionLanguageExecutor.start(code);
+    
+    return null;
 }
+

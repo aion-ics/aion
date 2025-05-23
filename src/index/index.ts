fe59513aa2ef41bce1c7d8677f188a464a82c5e5
Timur Cravtov <timurcravtov04@gmail.com>
2025-05-23 19:57:48 +0300

    Fixed npm entry point func logic

--- src/index/index.ts
+++ src/index/index.ts
@@ -7,17 +7,23 @@ import { IODictionarySystem } from "../core/intepreter/helpers/io_system/ioDicti
 
 
 /**
- * 
- * @param code The Aion source code to be executed.
+ * This piece of code starts compilation of Aion source file. In case you provide <code>aionSourceCode</code>, then it will be considered the aion script file. Otherwise, you include an Aion source file in <code>availableFiles</code>
+ *
  * @param filename The name of the file containing the Aion source code.
- * @param existingCalendars - A map of existing calendars (.ics) to be used with filename as a key (without the .ics extension) and the calendar code as a value.
- * @returns updated and created calendars in form of the map. 
+ * @param aionSourceCode - source code of Aion file.
+ * @param fileMap - A map of existing calendars (.ics) to be used with filename as a key (without the .ics extension) and the calendar code as a value. Can also include the aion script source code. If <code>aionSourceCode</code> is null, the source must be included in <code>availableFiles</code> under filename.
+ * @returns updated and created calendars in form of the map.
  */
-export function executeAionCode(code: string, filename: string = "input.aion", existingCalendars: Map<string, string>): Map<string, string> {
-    const ioSystem = new IODictionarySystem(existingCalendars);
-    const aionLanguageExecutor = new AionLanguageExecutor(ioSystem);
-    aionLanguageExecutor.start(filename);
+export function executeAionCode(filename: string, fileMap: Map<string, string> = new Map(), aionSourceCode: string = null, ): Map<string, string> {
 
-    return null;
+    if (aionSourceCode != null) {
+        fileMap.set(filename, aionSourceCode);
+    }
+
+    if (fileMap.has(filename)) {
+        const ioSystem = new IODictionarySystem(fileMap);
+        const aionLanguageExecutor = new AionLanguageExecutor(ioSystem);
+        aionLanguageExecutor.start(filename);
+    }
+    return fileMap;
 }
-
